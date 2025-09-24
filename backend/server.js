56866f93d6a6f05f858a90c690e8e93852b1b661
jaafarhamza <jaafar.hamza711@gmail.com>
2025-09-24 14:16:07 +0100

    Create Dockerfile and Docker-compose.yml with conxion avec server.js

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is runnooo!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
